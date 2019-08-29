69fd30ff97676a94e17171ab127cbb468b93bc46
Joshua Ven Honoridez <youzeiteikoku@gmail.com>
2019-08-29 21:42:30 +0800

    viewSubjects API added

--- Backend/app/tables/userTable.js
+++ Backend/app/tables/userTable.js
@@ -3,10 +3,18 @@ const pool = require('../../databasePool');
 class UserTable {
 	static viewSubjects() {
 		return new Promise((resolve, reject) => {
-			pool.query(`SELECT * FROM SUBJECTS`, (err, res) => {
-				if (err) return reject(err);
-				resolve(res.rows);
-			});
+			pool.query(
+				`SELECT "courseType", "acro", "subjectNum", "subjectDesc"
+				FROM subjectnum
+				inner join subjects
+				on subjectnum."subjectId" = subjects.id
+				inner join coursetype
+				on subjectnum."courseTypeId" = coursetype.id`,
+				(err, res) => {
+					if (err) return reject(err);
+					resolve(res.rows);
+				}
+			);
 		});
 	}
 }
