66bf3a2ee4b846949b9b2e902735e55ef7afd35c
Joshua Ven Honoridez <youzeiteikoku@gmail.com>
2019-08-29 16:20:22 +0800

    initialized the backend data

---
+++ Backend/bin/sql/courseType.sql
@@ -0,0 +1,5 @@
+CREATE TABLE courseType
+(
+    id SERIAL PRIMARY KEY,
+    "courseType" VARCHAR(32)
+)
---
+++ Backend/secrets/databaseConfiguration.js
@@ -0,0 +1,7 @@
+module.exports = {
+	user: 'scheduler_user',
+	host: 'localhost',
+	database: 'schedulerdb',
+	password: 'sc_password',
+	port: 5432
+};
