bf874f3bbe31e651b2363a3ee1cc7d59a6abd3ba
NishaAlexis <alexispadura@gmail.com>
2019-09-12 16:13:59 +0800

    updated index.html

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -1,21 +1,7 @@
 function proceed() {
-    var yrlevel = document.getElementById("year").value;
-    var semester = document.getElementById("sem").value;
-    document.getElementById("studentInfo").innerHTML = yrlevel + " Year    " + semester + " Semester";
-
-    document.getElementById("studentForms").style.display = "none";
-    document.getElementById("trigger").style.display = "none";
-
-    document.getElementById("hiddenCourse").style.display = "block";
-    document.getElementById("hiddenCourseOffers").style.display = "block";
+    
+  window.location.href = "pages/schedules.html";
 
   }
 
-
-function findSched() {
-  document.getElementById("hiddenCourse").style.display = "none";
-  document.getElementById("hiddenCourseOffers").style.display = "none";
-  
-  document.getElementById("hiddenSchedules").style.display = "block";
-}
 
