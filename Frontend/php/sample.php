0839991e65e36def7ab82fe671de706572dee064
Joshua Ven Honoridez <youzeiteikoku@gmail.com>
2019-09-19 19:11:14 +0800

    update on php files

--- Frontend/php/sample.php
+++ Frontend/php/sample.php
@@ -1,16 +1,26 @@
 <?php
+    $q = intval($_GET['q']);
+
     $db_connection = pg_connect("host=localhost dbname=schedulerdb user=scheduler_user password=sc_password");
     if (!$db_connection) {
-        echo "An error occurred.\n";
+        echo "<p>An error occurred.</p>";
         exit;
     } else {
-        echo "Success!";
+        echo "<p>Success!</p>";
     }
 
-    $result = pg_query($db_connection, "SELECT * FROM subjects");
+    function subjectsTable() {
+        $result = pg_query($db_connection, "SELECT * FROM subjects");
 
-    while ($row = pg_fetch_row($result)) {
-        echo "$row[0] - $row[1] - $row[2]";
-        echo "\n";
+        echo "<table id='tableSubjects'>";
+
+        while ($row = pg_fetch_row($result)) {
+            echo "<tr id='rowSubjects'>";
+            echo "<td id='subjectId'>$row[0]</td><td id='subjectName'>$row[1]</td><td id='subjectAcro'>$row[2]</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
     }
+
+
 ?>
