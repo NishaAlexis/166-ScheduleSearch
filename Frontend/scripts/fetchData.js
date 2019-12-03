3c2cca9cd2de33e55e6116b0c264554743e31380
Joshua Ven Honoridez <youzeiteikoku@gmail.com>
2019-12-03 20:09:49 +0800

    updated

--- Frontend/scripts/fetchData.js
+++ Frontend/scripts/fetchData.js
@@ -1,8 +1,4 @@
-var url = 'localhost:4000/user/viewSubjects';
-
-fetch(url, {
-	headers: {
-		'Content-Type': 'application/json',
-		host: 'localhost:4000'
-	}
-}).then((data) => console.log(data));
+function getParams() {
+	var yearData = document.getElementById('year').value;
+	console.log(yearData);
+}
--- Frontend/scripts/fetchJSON.js
+++ Frontend/scripts/fetchJSON.js
@@ -3,16 +3,31 @@ xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		const list = JSON.parse(this.responseText);
 
-		var cols = Headers(list, '#table');
-		var newList = getRandomSchedules(list, 3, 2);
+		// var parameters = location.search.substring(1).split('&');
+		// console.log(parameters);
+		// var temp = parameters[0].split('=');
+		// l = unescape(temp[1]);
+		// temp = parameters[1].split('=');
+		// p = unescape(temp[1]);
+
+		var cols = Headers(list, '#table'); //var cols = Headers(list, '#table');
+		console.log(cols);
+		var newList = getRandomSchedules(list, 2, 1);
 		for (var i = 0; i < newList.length; i++) {
 			var row = $('<tr/>');
+
+			var val = newList[i][9];
+
+			if (val == null) val = '';
+			row.append($('</td>').html(val));
+
 			for (var colIndex = 0; colIndex < cols.length; colIndex++) {
-				var val = newList[i][cols[colIndex]];
+				var val = '<input type="checkbox">' + newList[i][cols[colIndex]];
 
 				// If there is any key, which is matching
 				// with the column name
 				if (val == null) val = '';
+				console.log(val);
 				row.append($('<td/>').html(val));
 			}
 
@@ -23,6 +38,12 @@ xmlhttp.onreadystatechange = function() {
 		//ToDo
 		//Generate random schedules given year level and semester
 
+		function processUser() {
+			console.log(l);
+			document.getElementById('log').innerHTML = l;
+			document.getElementById('pass').innerHTML = p;
+		}
+
 		function Headers(list, selector) {
 			var columns = [];
 			var schedules = [];
@@ -31,18 +52,18 @@ xmlhttp.onreadystatechange = function() {
 			for (var i = 0; i < list.length; i++) {
 				var row = list[i];
 
-				for (var k in row) {
-					if ($.inArray(k, columns) == -1) {
-						columns.push(k);
-
-						// Creating the header
-						header.append($('<th/>').html(k));
-					}
+				// for (var k in row) {
+				if ($.inArray('abbr', columns) == -1) {
+					// k (header) is in columns (header array)
+					columns.push('abbr');
+					// Creating the header
+					header.append($('<th/>').html('abbr'));
 				}
+				// }
 				schedules.push(row);
 			}
 
-			// Appending the header to the table
+			//Appending the header to the table
 			$(selector).append(header);
 			return columns;
 		}
@@ -53,7 +74,7 @@ xmlhttp.onreadystatechange = function() {
 			var tempSchedules = [];
 			for (var i = 0; i < schedules.length; i++) {
 				// console.log(schedules[i]);
-				if (schedules[i].yearLvl === yearLevel && schedules[i].semester === semester) {
+				if (schedules[i].yearLvl == yearLevel && schedules[i].semester == semester) {
 					tempSchedules.push(schedules[i]);
 				}
 			}
